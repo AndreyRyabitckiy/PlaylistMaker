5fb1e504d64c4fb0a56e59ab04b54a6cea5da265
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-02-11 21:56:10 +0300

    0.6

---
+++ app/src/main/java/com/example/playlistmaker/MusicPlayer.kt
@@ -0,0 +1,79 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.Group
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class MusicPlayer : AppCompatActivity() {
+
+    private val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+
+        val backButton = findViewById<ImageView>(R.id.backIv)
+        val musicImage = findViewById<ImageView>(R.id.musicImageIv)
+        val nameMusic = findViewById<TextView>(R.id.nameMusicTv)
+        val groupName = findViewById<TextView>(R.id.groupNameTv)
+        val timeMusic = findViewById<TextView>(R.id.timeMusicAnswerTv)
+        val groupMusic = findViewById<TextView>(R.id.groupMusicAnswerTv)
+        val ear = findViewById<TextView>(R.id.earAnswerTv)
+        val typeMusic = findViewById<TextView>(R.id.typeMusicAnswerTv)
+        val country = findViewById<TextView>(R.id.countryAnswerTv)
+        val textGroup = findViewById<Group>(R.id.textGroup)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        intent.parcelable<Track>(TRACK_KEY)?.let { track ->
+
+            nameMusic.text = track.trackName
+            groupName.text = track.artistName
+            timeMusic.text = dateFormat.format(track.trackTimeMillis.toLong())
+            if (track.collectionName == null) {
+                textGroup.isVisible = false
+            } else {
+                groupMusic.text = track.collectionName
+            }
+            ear.text = getFormattedDate(track.releaseDate)
+            typeMusic.text = track.primaryGenreName
+            country.text = track.country
+            val urlImage = track.artworkUrl100?.replaceAfterLast('/', "512x512bb.jpg")
+
+            Glide.with(this)
+                .load(urlImage)
+                .placeholder(R.drawable.placeholder_ic)
+                .centerInside()
+                .transform(RoundedCorners(10))
+                .into(musicImage)
+        }
+    }
+
+    private fun getFormattedDate(inputDate: String?): String {
+        inputDate ?: return ""
+
+        val dfIn = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+        val dfOut = SimpleDateFormat("yyyy", Locale.getDefault())
+
+        dfIn.parse(inputDate)?.let { date ->
+            return dfOut.format(date) ?: ""
+        } ?: return ""
+    }
+
+    companion object {
+        const val TRACK_KEY = "TRACK"
+    }
+}
+
+
--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -6,10 +6,16 @@ import android.widget.Adapter
 import androidx.recyclerview.widget.RecyclerView
 import java.util.ArrayList
 
-class TrackAdapter(
-    private val data: ArrayList<Track>,
-    private val onItemClickListener: OnItemClickListener? = null
-) : RecyclerView.Adapter<TrackViewHolder>() {
+class TrackAdapter : RecyclerView.Adapter<TrackViewHolder>() {
+
+    var data: ArrayList<Track> = arrayListOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    var onClick: (Track) -> Unit = {}
+
 
     override fun getItemCount(): Int {
         return data.size
@@ -17,12 +23,11 @@ class TrackAdapter(
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(data[position])
-        holder.itemView.setOnClickListener { onItemClickListener?.onItemClick(data[position]) }
+        holder.itemView.setOnClickListener { onClick(data[position]) }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
         return TrackViewHolder(view)
     }
-
 }
