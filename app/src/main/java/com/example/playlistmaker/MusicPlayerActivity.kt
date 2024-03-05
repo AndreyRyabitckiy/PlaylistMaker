4ed2c202f8d8ee96bbf7949ec504138bcead3378
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-03-05 14:26:35 +0300

    0.7

--- app/src/main/java/com/example/playlistmaker/MusicPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MusicPlayerActivity.kt
@@ -1,20 +1,43 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.Group
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.android.material.button.MaterialButton
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class MusicPlayerActivity : AppCompatActivity() {
 
+    companion object {
+        const val TRACK_KEY = "TRACK"
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+        private const val DELAY = 300L
+        private const val MUSIC_TIME = 29900
+        private const val TIME_START = "00:00"
+    }
+
+    private val mainThreadHandler by lazy { Handler(Looper.getMainLooper()) }
+
     private val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+    private var playerState = STATE_DEFAULT
+    private var mediaPlayer = MediaPlayer()
+    private var url: String? = " "
+    private lateinit var playButton: MaterialButton
+    private val timeMusic30 by lazy { findViewById<TextView>(R.id.timeMusic30Tv) }
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,12 +55,14 @@ class MusicPlayerActivity : AppCompatActivity() {
         val country = findViewById<TextView>(R.id.countryAnswerTv)
         val textGroup = findViewById<Group>(R.id.textGroup)
 
+        playButton = findViewById(R.id.playMusicB)
+
         backButton.setOnClickListener {
             finish()
         }
 
         intent.parcelable<Track>(TRACK_KEY)?.let { track ->
-
+            url = track.previewUrl
             nameMusic.text = track.trackName
             groupName.text = track.artistName
             timeMusic.text = dateFormat.format(track.trackTimeMillis.toLong())
@@ -58,6 +83,47 @@ class MusicPlayerActivity : AppCompatActivity() {
                 .transform(RoundedCorners(radiusCutImage))
                 .into(musicImage)
         }
+
+        preparePlayer()
+        playButton.setOnClickListener {
+            startTimerMusic()
+            playbackControl()
+        }
+    }
+
+    private fun startTimerMusic() {
+        if (playerState == STATE_PREPARED || playerState == STATE_PAUSED)
+            mainThreadHandler?.post(
+                createUpdateTimerMusicTask()
+            ) else {
+            mainThreadHandler.removeCallbacksAndMessages(null)
+        }
+    }
+
+    private fun createUpdateTimerMusicTask(): Runnable {
+        return object : Runnable {
+            override fun run() {
+                val timeMusicAnswer = mediaPlayer.currentPosition
+                if (timeMusicAnswer < MUSIC_TIME) {
+                    timeMusic30.text = dateFormat.format(timeMusicAnswer.toLong()).toString()
+                    mainThreadHandler?.postDelayed(this, DELAY)
+                } else {
+                    timeMusic30.text = TIME_START
+                    mainThreadHandler?.postDelayed(this, DELAY)
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        mediaPlayer.release()
+        mainThreadHandler.removeCallbacksAndMessages(null)
+        super.onDestroy()
     }
 
     private fun getFormattedDate(inputDate: String?): String {
@@ -71,8 +137,42 @@ class MusicPlayerActivity : AppCompatActivity() {
         } ?: return ""
     }
 
-    companion object {
-        const val TRACK_KEY = "TRACK"
+    @SuppressLint("SetTextI18n")
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playerState = STATE_PREPARED
+        }
+
+        mediaPlayer.setOnCompletionListener {
+            playButton.setIconResource(R.drawable.ic_play)
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.setIconResource(R.drawable.pause_ic)
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.setIconResource(R.drawable.ic_play)
+        playerState = STATE_PAUSED
     }
 }
 
